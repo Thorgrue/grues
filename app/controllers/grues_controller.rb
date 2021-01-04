fef31886568271c9c3f1fc4a51e95ccafee9c6f6
Gagou <gaelle.shearn.jmr@gmail.com>
2021-01-04 23:09:10 +0100

    r

--- app/controllers/grues_controller.rb
+++ app/controllers/grues_controller.rb
@@ -1,4 +1,5 @@
 class GruesController < ApplicationController
+  before_action :set_grue, only: [:show, ]
 
   def index
     @grues = Grue.all
@@ -11,5 +12,39 @@ class GruesController < ApplicationController
         infoWindow: render_to_string(partial: 'info_window', locals: { grue: grue })
       }
     end
+  end
+
+  def show
+  end
+
+  def new
+    @grue = Grue.new
+  end
+
+  def create
+    @grue = Grue.new(grue_params)
+    if @grue.save
+      redirect_to @grue
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+  private
+
+  def set_grue
+    @grue = Grue.find(params[:id])
+  end
+
+  def grue_params
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  get 'grues/index', to: 'grues#index', as: 'grues_index'
-  # get 'pages/search', to: 'pages#search', as: 'search'
-  # get 'cities/map', to: 'cities#map', as: 'cities_map '
+  resources :grues, only: [:index, :show, :create, :new, :update, :edit] do
+  end
 end
